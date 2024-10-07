da61c02c8f4ef86eb424601ce8cf3da90d8e7f5d
MuhRafiq28 <rafiqthebungsu@gmiail.com>
2024-10-07 14:50:04 +0700

    membuat daftra film

--- components/Daftar-film.vue
+++ components/Daftar-film.vue
@@ -1,36 +1,23 @@
 <template>
-  <div class="container p-4 mt-3 m-lg-5">
-    <div class="judul">
+  <div class="container p-4">
+    <div class="judul text-center">
       <h1>Film Rekomendasi</h1>
     </div>
-    <div class="d-flex justify-content-between">
-      <div class="container-car d-flex pt-4 pb-4 align-content-center">
-        <div class="gambar-poster d-flex align-items-center">
-          <img class="poster" src="images/godzila.jpg" alt="" />
-        </div>
-        <div class="descripsi-poster ml-2 p-4 ">
-          <h1><strong> Godzila X Kong </strong></h1>
-          <p class="m-0"><strong> Si Botak </strong></p>
-          <p class="m-0">Konoha</p>
-          <p class="mt-4">
-            cerita tentang monster yang hidup di intin bumi yang kemudian hari
-            naik ke permukaan bumi yaitu tempat manusia hidup
-          </p>
-        </div>
-      </div>
-
-      <div class="container-car d-flex pt-4 pb-4 align-content-center ml-3">
-        <div class="gambar-poster">
-          <img class="poster" src="images/godzila.jpg" alt="" />
-        </div>
-        <div class="descripsi-poster ml-2 p-4">
-          <h1><strong> Godzila X Kong </strong></h1>
-          <p class="m-0"><strong> Si Botak </strong></p>
-          <p class="m-0">Konoha</p>
-          <p class="mt-4">
-            cerita tentang monster yang hidup di intin bumi yang kemudian hari
-            naik ke permukaan bumi yaitu tempat manusia hidup
-          </p>
+    <div class="slideshow-container">
+      <div class="row">
+        <div
+          class="col-6"
+          v-for="(film, index) in displayedFilms"
+          :key="index"
+        >
+          <div class="film-card  ">
+            <img class="poster" :src="film.poster" alt="" />
+            <div class="film-details ml-3">
+              <h2 class="film-title"><strong>{{ film.judul }}</strong></h2>
+              <p class="film-maker"><strong>{{ film.pembuat }}</strong></p>
+              <p class="film-description">{{ film.deskripsi }}</p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -39,7 +26,56 @@
 
 <script>
 export default {
-  nama: "DaftarFilm",
+  name: "DaftarFilm",
+  data() {
+    return {
+      films: [
+        {
+          judul: "Godzila X Kong",
+          pembuat: "Si Botak",
+          deskripsi: "Cerita tentang monster yang hidup di dalam bumi dan naik ke permukaan.",
+          poster: "images/godzila.jpg",
+        },
+        {
+          judul: "Film Lain",
+          pembuat: "Si Botak",
+          deskripsi: "Deskripsi untuk film lain.",
+          poster: "images/godzila.jpg",
+        },
+        {
+          judul: "Film Ketiga",
+          pembuat: "Si Botak",
+          deskripsi: "Deskripsi untuk film ketiga.",
+          poster: "images/godzila.jpg",
+        },
+        {
+          judul: "Film Keempat",
+          pembuat: "Si Botak",
+          deskripsi: "Deskripsi untuk film keempat.",
+          poster: "images/godzila.jpg",
+        },
+      ],
+      currentSlide: 0,
+    };
+  },
+  computed: {
+    displayedFilms() {
+      return [
+        this.films[this.currentSlide % this.films.length],
+        this.films[(this.currentSlide + 1) % this.films.length],
+      ];
+    },
+  },
+  mounted() {
+    this.startSlideshow();
+  },
+  methods: {
+    startSlideshow() {
+      setInterval(() => {
+        this.currentSlide = (this.currentSlide + 1) % this.films.length;
+      }, 3000);
+    },
+  },
 };
 </script>
 
@@ -48,5 +84,57 @@ export default {
   width: 140px;
   height: auto;
   border-radius: 10px;
+}
+.slideshow-container {
+  position: relative;
+  max-width: 800px;
+  margin: auto;
+}
+.row {
+  display: flex;
+  flex-wrap: wrap;
+}
+.col-6 {
+  width: 50%;
+  padding: 10px;
+}
+.film-card {
+  display: flex;
+  align-items: flex-start;
+  padding: 10px;
+  border: 1px solid #ddd;
+  border-radius: 8px;
+  background-color: #f9f9f9;
+  margin-left: 50px;
+}
+.film-details {
+  margin-left: 15px;
+  flex: 1;
+  margin-top: 10px;
+}
+.film-title {
+  font-size: 1.25rem;
+  margin-bottom: 5px;
+}
+.film-maker, .film-description {
+  margin: 0;
+}
+
+
+@media (max-width: 1100px) {
+  .col-6 {
+    width: 100%;
+  }
+}
+
+
+@media (max-width: 768px) {
+  .film-card {
+    display: block;
+    align-items: center;
+  }
+  .poster {
+    width: 80%;
+  }
 }
 </style>
